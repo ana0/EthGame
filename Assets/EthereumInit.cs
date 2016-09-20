cb5e8191c48bf6566a9b3ed063366294317231fe
ana0 <sarah.anne.friend@gmail.com>
2016-09-20 19:58:44 -0400

    Begin parsing json and return response from Coroutine

--- Assets/EthereumInit.cs
+++ Assets/EthereumInit.cs
@@ -1,22 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 using UnityEngine.Networking;
 
 public class EthereumInit : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-//		NetworkTransport.Init();
-//		ConnectionConfig config = new ConnectionConfig();
-//		int myReliableChannelId  = config.AddChannel(QosType.Reliable);
-//		int myUnreliableChannelId = config.AddChannel(QosType.Unreliable);
-//		HostTopology topology = new HostTopology(config, 10);
-//		int hostId = NetworkTransport.AddHost(topology, 8888);
-//
-//		connectionId = NetworkTransport.Connect(hostId, "192.16.7.21", 8888, 0, out error);
-		StartCoroutine(Test());
+		var _params = new Params[] {};
 
+		var json = new Jsonify();
+		json.jsonrpc = "2.0";
+		json.method = "eth_coinbase";
+		json.@params = _params;
+		json.id = 64;
+
+		string Jsonified = JsonUtility.ToJson(json);
+
+		var _dejson = new DeJson();
+
+		StartCoroutine(Call(Jsonified, setRect));
+
+//		print (_dejson.text);
 	}
+
 	IEnumerator Test() {
 		string jsonstring = "{\"jsonrpc\":\"2.0\",\"method\":\"eth_coinbase\",\"params\":[],\"id\":64}";
 		var encoding = new System.Text.UTF8Encoding();
@@ -26,18 +33,87 @@ public class EthereumInit : MonoBehaviour {
 		headers["Content-type"] =  "text/json";
 		byte[] data = form.data;
 		data = encoding.GetBytes (jsonstring);
-		//form.AddField ("{'jsonrpc':\"2.0\",\"method\":\"eth_coinbase\",\"params\":[],\"id\":64}'", "'{\"jsonrpc\":\"2.0\",\"method\":\"eth_coinbase\",\"params\":[],\"id\":64}'");
+
+		WWW www = new WWW ("http://localhost:8080/rpc", data, headers);
+
+		yield return www;
+	}
+
+	[System.Serializable]
+	public struct Jsonify {
+		public string jsonrpc;
+		public string method;
+		public Params[] @params;
+		public int id;
+	}
+
+	[System.Serializable]
+	public struct Params {
+		public string from;
+		public string to;
+		public string gas;
+		public string gasPrice;
+		public string value;
+		public string data;
+		public string nonce;
+	}
+
+	[System.Serializable]
+	public class DeJson {
+		public string text;
+	}
+
+//	public class CoroutineWithData {
+//		public Coroutine coroutine { get; private set; }
+//		public object result;
+//		private IEnumerator target;
+//		public CoroutineWithData(MonoBehaviour owner, IEnumerator target) {
+//			this.target = target;
+//			this.coroutine = owner.StartCoroutine(Run());
+//		}
+//
+//		private IEnumerator Run() {
+//			while(target.MoveNext()) {
+//				result = target.Current;
+//				yield return result;
+//			}
+//		}
+//	}
+
+	IEnumerator Call(string jsonstring, Action<string> resultCallback) {
+		var encoding = new System.Text.UTF8Encoding();
+
+		WWWForm form = new WWWForm();
+		var headers = form.headers;
+		headers["Content-type"] =  "text/json";
+		byte[] data = form.data;
+		data = encoding.GetBytes (jsonstring);
 
 		WWW www = new WWW ("http://localhost:8080/rpc", data, headers);
 		yield return www;
-		print (form.headers);
-		print (form.data);
-		print (www.text);
+		string newRect = www.text;
+		resultCallback(newRect);
+
+		yield break;
+		//		resultCallback = www.text;
+		//		yield return result.text;
+
 	}
+		
+
+	public string response;
+
+	public void setRect(string _response){
+		response = _response;
+	}
+
 
 	// Update is called once per frame
 	void Update () {
-	
+//		print (response);
 	}
 }
+
+
+
 
